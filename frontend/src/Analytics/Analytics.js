57bb097c9512faa98469f7d9f9b366bc4ca424d4
fulpopy <akash16799@gmail.com>
2023-05-13 02:23:42 +0530

    second commit

---
+++ frontend/src/Analytics/Analytics.js
@@ -0,0 +1,68 @@
+import React from "react";
+import BarGraph from "./BarGraph";
+import { Card, Typography } from "@material-ui/core";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import "./styles.css";
+const newPartCarbonFoot = [735.5, 932, 754, 426, 804];
+const recycledPartCarbonFoot = [150.5, 43.5, 113, 162, 128];
+const waterUsageNewPart = [7516, 8192, 8605, 4986, 1874];
+const waterUsageRecycledPart = [344, 575.5, 235, 800, 249];
+const landfillWasteNewPart = [513, 225, 716, 455, 353];
+const landfillWasteRecycledPart = [132.5, 59.5, 96, 185, 60];
+const energyConsumptionNewPart = [2409.5, 1909.5, 4298, 4554, 744];
+const energyConsumptionRecycledPart = [117.5, 276, 285, 175, 228];
+const units = [
+  "Average Corbon Footprint(kg CO2e)",
+  "Average Water Saved(liters)",
+  "Avg Landfill Waste(kg)",
+  "Avg Energy Consumption(kWh)",
+];
+const Analytics = () => {
+  return (
+    <div className="grid">
+      <div className="grid-item">
+        <Card>
+          <BarGraph
+            d1={newPartCarbonFoot}
+            d2={recycledPartCarbonFoot}
+            unit={units[0]}
+          />
+        </Card>
+        <Typography variant="h5" component="h5">
+          Carbon Footprint
+        </Typography>
+      </div>
+      <div className="grid-item">
+        <Card>
+          <BarGraph d1={waterUsageNewPart} d2={waterUsageRecycledPart} unit={units[1]}/>
+        </Card>
+        <Typography variant="h5" component="h5">
+          Water Usage
+        </Typography>
+      </div>
+      <div className="grid-item">
+        <Card>
+          <BarGraph d1={landfillWasteNewPart} d2={landfillWasteRecycledPart} unit={units[2]}/>
+        </Card>
+        <Typography variant="h5" component="h5">
+          Landfill Wastet
+        </Typography>
+      </div>
+      <div className="grid-item">
+        <Card>
+          <BarGraph
+            d1={energyConsumptionNewPart}
+            d2={energyConsumptionRecycledPart}
+            unit={units[3]}
+          />
+        </Card>
+        <Typography variant="h5" component="h5">
+          Energy Consumption
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default Analytics;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,14 @@ import Home from "./Components/Home";
 import Admin from "./Components/Admin/Admin";
 import PrivateRoutes from "./Components/PrivateRoutes";
 import { AuthProvider } from "./Auth";
+import Analytics from "./Analytics/Analytics";
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SignIn />} />
+          <Route path="/analytics" element={<Analytics />} />
           <Route path="/user" element={<PrivateRoutes />}>
             <Route path="admin" element={<Admin />} />
           </Route>
